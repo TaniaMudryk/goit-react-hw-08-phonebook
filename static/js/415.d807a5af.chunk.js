47640cc280d695d761c60c6971d06083de0119ef
TaniaMudryk <99959425+TaniaMudryk@users.noreply.github.com>
2023-05-19 15:36:37 +0000

    Deploying to gh-pages from @ TaniaMudryk/goit-react-hw-08-phonebook@ac9279331fa48008eab821c380966cbbbdba28d9 🚀

---
+++ static/js/415.d807a5af.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[415],{5415:function(e,n,o){o.r(n),o.d(n,{default:function(){return t}});var r=o(9576),i=o(3329);function t(){return(0,i.jsx)(r.W2,{children:(0,i.jsxs)("h1",{children:["Phonebook"," ",(0,i.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\u260e\ufe0f"})]})})}}}]);
+//# sourceMappingURL=415.d807a5af.chunk.js.map
